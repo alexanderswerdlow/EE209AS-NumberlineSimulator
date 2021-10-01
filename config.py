7fd0e9b863cede19800a4dbb7371443932d25c12
Alexander Swerdlow <aswerdlow1@gmail.com>
2021-10-01 16:44:24 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,39 @@
+import numpy as np
+import random
+from scipy.misc import derivative
+
+v_max = 10
+p_c = 5
+m = 10
+
+def phi(y):
+    return -2 * np.sin(y)
+
+def u(t):
+    return 0.5 * np.sqrt(t)
+
+def f(state, u, t):
+    y_new = state[0] + state[1]
+    has_crashed = random.uniform(0, 1) < ((np.abs(state[1]) - v_max) * p_c)/v_max
+    if has_crashed:
+        v_new = 0
+    else:
+        v_new = state[1] + (1 / m) * (u - phi(state[0])) + + np.random.normal(0, 0.1)
+
+    return np.vstack((y_new, v_new))
+
+def h(state, input, t):
+    return state[0] + np.random.normal(0, 0.5)
+
+x = np.vstack((5, 10))
+
+
+
+
+
+
+
+
+
+
+
---
+++ simulator.py
@@ -0,0 +1,42 @@
+from config import x, u, f, h
+
+class Simulator:
+
+    def __init__(self, dt, x, u, f, h) -> None:
+        self.t = 0
+        self.dt = dt
+        self.x = x
+        self.u = u
+        self.f = f
+        self.h = h
+
+    def step(self):
+        self.x = f(self.x, self.u(self.t), self.t)
+        self.t += self.dt
+
+    def get_state(self):
+        return self.x
+
+    def get_observation(self):
+        return h(self.x, self.u(self.t), self.t)
+
+    def get_time(self):
+        return self.t
+
+
+sim = Simulator(0.1, x, u, f, h)
+position, velocity, measured_velocity, time = [], [], [], []
+for i in range(100):
+    position.append(sim.get_state()[0])
+    velocity.append(sim.get_state()[1])
+    measured_velocity.append(sim.get_observation())
+    time.append(sim.get_time())
+    print(sim.get_state())
+    sim.step()
+
+import matplotlib.pyplot as plt
+plt.plot(time, position)
+plt.plot(time, velocity)
+plt.plot(time, measured_velocity)
+plt.xlabel("Time")
+plt.show()
